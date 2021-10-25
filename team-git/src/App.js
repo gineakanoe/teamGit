eac21ad6b374e7a46eb7390a1cfa36acea079927
Ginea Merrill <gineakanoe@gmail.com>
2021-10-25 16:55:43 -0400

    changed tab title in index html and imported and added basic wireframe to app js page

--- team-git/src/App.js
+++ team-git/src/App.js
@@ -1,8 +1,31 @@
+import React from 'react';
 import './App.css';
+import Nasa from './component/app/Nasa';
+import Weatherapp from './component/app/Weatherapp';
+import Ticketmaster from './component/app/Ticketmaster';
 
 function App() {
   return (
     <div className="App">
+      <h1>Master Branch</h1>
+      <br />
+      <h2>Develop Branch</h2>
+      <br />
+      <div>
+        <h3>Reagan Branch</h3>
+        <h3>Nasa</h3>
+        {/* <Nasa /> */}
+      </div>
+      <div>
+        <h3>Nolan Branch</h3>
+        <h3>Weather</h3> 
+        {/* <Weatherapp /> */}
+      </div>
+      <div>
+        <h3>Ginea Branch</h3>
+        <h3>Ticketmaster</h3>
+        {/* <Ticketmaster /> */}
+      </div>
 
     </div>
   );
